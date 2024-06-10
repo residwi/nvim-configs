9892a8d60889ced80e099a0955d6efe56345f662
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-11 02:48:44 +0700

    Add smartindent, colorcolumn and termguicolors

--- lua/nvim-configs/editor.lua
+++ lua/nvim-configs/editor.lua
@@ -7,6 +7,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.breakindent = true
+vim.opt.smartindent = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
@@ -28,3 +29,8 @@ vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
+
+-- Correct terminal colors
+vim.opt.termguicolors = true
+
+vim.opt.colorcolumn = "80"
