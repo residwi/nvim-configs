c0ed25e87705782ad5253d49b92296ff476877d1
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-13 18:40:32 +0700

    Remove redundant dependency for nvim-treesitter-textobjects

--- lua/nvim-configs/plugins/surround.lua
+++ lua/nvim-configs/plugins/surround.lua
@@ -2,9 +2,6 @@ return {
 	"kylechui/nvim-surround",
 	version = "*",
 	event = "VeryLazy",
-	dependencies = {
-		"nvim-treesitter/nvim-treesitter-textobjects",
-	},
 	config = function()
 		require("nvim-surround").setup({})
 	end,
