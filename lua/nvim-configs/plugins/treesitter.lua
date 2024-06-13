ac4cbd22254b59bbb6143c60ef9380b33b1dd6d0
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-13 18:36:42 +0700

    Add treesitter-textobjects

--- lua/nvim-configs/plugins/treesitter.lua
+++ lua/nvim-configs/plugins/treesitter.lua
@@ -3,6 +3,7 @@ return {
 	build = ":TSUpdate",
 	dependencies = {
 		"RRethy/nvim-treesitter-endwise",
+		"nvim-treesitter/nvim-treesitter-textobjects",
 	},
 
 	opts = {
@@ -18,6 +19,42 @@ return {
 			additional_vim_regex_highlighting = { "ruby" },
 		},
 		indent = { enable = true, disable = { "ruby" } },
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true,
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+					["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+				},
+				-- You can choose the select mode (default is charwise 'v')
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * method: eg 'v' or 'o'
+				-- and should return the mode ('v', 'V', or '<c-v>') or a table
+				-- mapping query_strings to modes.
+				selection_modes = {
+					["@parameter.outer"] = "v", -- charwise
+					["@function.outer"] = "V", -- linewise
+					["@class.outer"] = "<c-v>", -- blockwise
+				},
+				-- If you set this to `true` (default is `false`) then any textobject is
+				-- extended to include preceding or succeeding whitespace. Succeeding
+				-- whitespace has priority in order to act similarly to eg the built-in
+				-- `ap`.
+				--
+				-- Can also be a function which gets passed a table with the keys
+				-- * query_string: eg '@function.inner'
+				-- * selection_mode: eg 'v'
+				-- and should return true or false
+				include_surrounding_whitespace = true,
+			},
+		},
 	},
 	config = function(_, opts)
 		-- Prefer git instead of curl in order to improve connectivity in some environments
