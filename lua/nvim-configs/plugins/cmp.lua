09935e87f986c1c8c8fea384b56e7f5b46f5df33
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-13 18:42:00 +0700

    Remapping confirm keymap for completion and add snippet plugin

--- lua/nvim-configs/plugins/cmp.lua
+++ lua/nvim-configs/plugins/cmp.lua
@@ -14,22 +14,15 @@ return {
 				return "make install_jsregexp"
 			end)(),
 			dependencies = {
-				-- `friendly-snippets` contains a variety of premade snippets.
-				--    See the README about individual language/framework/plugin snippets:
-				--    https://github.com/rafamadriz/friendly-snippets
-				-- {
-				--   "rafamadriz/friendly-snippets",
-				--   config = function()
-				--     require("luasnip.loaders.from_vscode").lazy_load()
-				--   end,
-				-- },
+				{
+					"rafamadriz/friendly-snippets",
+					config = function()
+						require("luasnip.loaders.from_vscode").lazy_load()
+					end,
+				},
 			},
 		},
 		"saadparwaiz1/cmp_luasnip",
-
-		-- Adds other completion capabilities.
-		--  nvim-cmp does not ship with all sources by default. They are split
-		--  into multiple repos for maintenance purposes.
 		"hrsh7th/cmp-nvim-lsp",
 		"hrsh7th/cmp-path",
 		"hrsh7th/cmp-buffer",
@@ -52,19 +45,13 @@ return {
 			--
 			-- No, but seriously. Please read `:help ins-completion`, it is really good!
 			mapping = cmp.mapping.preset.insert({
-				-- Select the [n]ext item
 				["<C-n>"] = cmp.mapping.select_next_item(),
-				-- Select the [p]revious item
 				["<C-p>"] = cmp.mapping.select_prev_item(),
 
 				-- Scroll the documentation window [b]ack / [f]orward
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
-
-				-- Accept ([y]es) the completion.
-				--  This will auto-import if your LSP supports it.
-				--  This will expand snippets if the LSP sent a snippet.
-				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
 
 				-- If you prefer more traditional completion keymaps,
 				-- you can uncomment the following lines
