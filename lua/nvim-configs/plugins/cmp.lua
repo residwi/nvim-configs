f2494ada36265318b87d57873712bde68f289c3e
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-19 17:18:57 +0700

    Show preview popup for completion

--- lua/nvim-configs/plugins/cmp.lua
+++ lua/nvim-configs/plugins/cmp.lua
@@ -38,7 +38,7 @@ return {
 					luasnip.lsp_expand(args.body)
 				end,
 			},
-			completion = { completeopt = "menu,menuone,noinsert" },
+			completion = { completeopt = "menu,menuone,preview,noinsert" },
 
 			-- For an understanding of why these mappings were
 			-- chosen, you will need to read `:help ins-completion`
