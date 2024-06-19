481d2e8da3af642f8780bf9b409ab63fc20bab93
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-19 17:38:03 +0700

    Make the lua lsp recognize vim global variable

--- lua/nvim-configs/plugins/lsp.lua
+++ lua/nvim-configs/plugins/lsp.lua
@@ -173,16 +173,16 @@ return {
 			--
 
 			lua_ls = {
-				-- cmd = {...},
-				-- filetypes = { ...},
-				-- capabilities = {},
 				settings = {
 					Lua = {
 						completion = {
 							callSnippet = "Replace",
 						},
-						-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-						-- diagnostics = { disable = { "missing-fields" } },
+						diagnostics = {
+							-- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+							-- disable = { "missing-fields" },
+							globals = { "vim" },
+						},
 					},
 				},
 			},
