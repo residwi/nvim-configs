830dc245fc4db25ac5189aed7a026c0e56a036d1
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-06-20 22:08:00 +0700

    Update theme to onedark pro

--- lua/nvim-configs/plugins/colorscheme.lua
+++ lua/nvim-configs/plugins/colorscheme.lua
@@ -1,7 +1,29 @@
 return {
-	"folke/tokyonight.nvim",
+	"olimorris/onedarkpro.nvim",
 	priority = 1000,
 	init = function()
-		vim.cmd.colorscheme("tokyonight-storm")
+		local color = require("onedarkpro.helpers")
+
+		require("onedarkpro").setup({
+			colors = {
+				dark = {
+					telescope_selection = color.lighten("bg", 2, "onedark"),
+				},
+			},
+			highlights = {
+				-- Telescope
+				TelescopeBorder = { fg = "${orange}", bg = "${float_bg}" },
+				TelescopePromptCounter = { bg = "${float_bg}", fg = "${gray}" },
+				TelescopePromptPrefix = { fg = "${blue}" },
+				TelescopeSelectionCaret = { fg = "${blue}" },
+				TelescopeSelection = { bg = "${telescope_selection}" },
+
+				-- Editor
+				Cursor = { bg = color.lighten("white", 2, "onedark"), fg = "${bg}" },
+				CursorLineNr = { fg = "${blue}" },
+			},
+		})
+
+		vim.cmd.colorscheme("onedark_vivid")
 	end,
 }
