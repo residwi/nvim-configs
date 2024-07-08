f082e5233795a2c7fef84d04fa92fba388271bdc
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-07-08 09:36:31 +0700

    Add plugin vim-tmux-navigator

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -15,3 +15,9 @@ vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 -- Centered vertical scrolling
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
