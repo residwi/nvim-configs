15b431387d6a67d3ae87f5668c77d734d2dcc3dc
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-10-13 21:46:32 +0700

    Refactor cmp sources to be merged with parent sources for better maintainability and structure

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -12,4 +12,14 @@ return {
 	},
 	-- Manage libuv types with lazy. Plugin will never be loaded
 	{ "Bilal2453/luvit-meta", lazy = true },
+	-- Add lazydev source to cmp
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+	},
 }
