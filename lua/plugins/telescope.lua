33245783c87a0f9f5647059c59707b7a8d152312
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-10-22 23:37:54 +0700

    Update ctrl+p keymap to find_files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -35,8 +35,7 @@ return {
 		pcall(require("telescope").load_extension, "ui-select")
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<C-p>", builtin.git_files)
-		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "[F]ind [F]iles" })
+		vim.keymap.set("n", "<C-p>", builtin.find_files)
 		vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "[F]ind [G]rep String" })
 		vim.keymap.set("n", "<leader>fb", function()
 			builtin.buffers({
