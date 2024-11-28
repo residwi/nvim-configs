2880eb8e7060f1c3d80cac7d27dd2d1d06dc21d8
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-11-28 19:04:34 +0700

    Update copilot plugin configuration

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,36 +1,53 @@
 return {
-	{
-		"zbirenbaum/copilot.lua",
-		cmd = "Copilot",
-		event = "InsertEnter",
-		config = function()
-			require("copilot").setup({
-				suggestion = { enabled = false },
-				panel = { enabled = false },
-				filetypes = {
-					markdown = true,
-					help = true,
-				},
-			})
-		end,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			{
-				"zbirenbaum/copilot-cmp",
-				dependencies = "copilot.lua",
-				config = function()
-					require("copilot_cmp").setup()
-				end
-			},
-		},
-		opts = function(_, opts)
-			table.insert(opts.sources, 1, {
-				name = "copilot",
-				group_index = 1,
-				priority = 100,
-			})
-		end,
-	},
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          help = true,
+        },
+      })
+    end,
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    optional = true,
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        dependencies = "copilot.lua",
+        opts = {},
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          local lsp_util = require("plugins.lsp.util")
+          lsp_util.on_attach(function()
+            copilot_cmp._on_insert_enter({})
+          end, "copilot")
+        end,
+        specs = {
+          {
+            "hrsh7th/nvim-cmp",
+            optional = true,
+            ---@param opts cmp.ConfigSchema
+            opts = function(_, opts)
+              table.insert(opts.sources, 1, {
+                name = "copilot",
+                group_index = 1,
+                priority = 100,
+              })
+            end,
+          },
+        },
+      },
+    },
+  },
 }
