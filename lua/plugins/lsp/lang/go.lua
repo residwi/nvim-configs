b6724b9b8eef77c5757882f9b299d6d8318c5d19
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-11-28 18:15:22 +0700

    Enable lazy loading for go.nvim plugin

--- lua/plugins/lsp/lang/go.lua
+++ lua/plugins/lsp/lang/go.lua
@@ -1,11 +1,11 @@
 return {
 	"ray-x/go.nvim",
+	lazy = true,
 	dependencies = {
 		"ray-x/guihua.lua",
 		"neovim/nvim-lspconfig",
 		"nvim-treesitter/nvim-treesitter",
 	},
-	event = { "CmdlineEnter" },
 	ft = { "go", "gomod" },
 	config = function(_, opts)
 		require("go").setup(opts)
