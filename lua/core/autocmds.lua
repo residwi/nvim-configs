1f0580b48a201044687c5e11ac92ed4f61e2230c
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-11-28 18:13:31 +0700

    Refactor autocmd for optimized file loading and support LazyFile event

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -2,6 +2,31 @@ local function augroup(name)
 	return vim.api.nvim_create_augroup(name, { clear = true })
 end
 
+-- This autocmd will only trigger when a file was loaded from the cmdline.
+-- It will render the file as quickly as possible.
+vim.api.nvim_create_autocmd("BufReadPost", {
+	once = true,
+	callback = function(event)
+		-- Skip if we already entered vim
+		if vim.v.vim_did_enter == 1 then
+			return
+		end
+
+		-- Try to guess the filetype (may change later on during Neovim startup)
+		local ft = vim.filetype.match({ buf = event.buf })
+		if ft then
+			-- Add treesitter highlights and fallback to syntax
+			local lang = vim.treesitter.language.get_lang(ft)
+			if not (lang and pcall(vim.treesitter.start, event.buf, lang)) then
+				vim.bo[event.buf].syntax = ft
+			end
+
+			-- Trigger early redraw
+			vim.cmd([[redraw]])
+		end
+	end,
+})
+
 -- Highlight on yank
 vim.api.nvim_create_autocmd("TextYankPost", {
 	desc = "Highlight when yanking (copying) text",
