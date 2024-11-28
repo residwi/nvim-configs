c8f8cbacee313c38b21e247444b8cfadad5d6f52
Resi Dwi <37761892+residwi@users.noreply.github.com>
2024-11-28 18:26:23 +0700

    Update nvim-cmp configuration with native snippet support

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,99 +1,118 @@
 return {
-	"hrsh7th/nvim-cmp",
-	event = "InsertEnter",
-	dependencies = {
-		{
-			"L3MON4D3/LuaSnip",
-			build = (function()
-				-- Build Step is needed for regex support in snippets.
-				-- This step is not supported in many windows environments.
-				-- Remove the below condition to re-enable on windows.
-				if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
-					return
-				end
-				return "make install_jsregexp"
-			end)(),
-			dependencies = {
-				{
-					"rafamadriz/friendly-snippets",
-					config = function()
-						require("luasnip.loaders.from_vscode").lazy_load()
-					end,
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-buffer",
+		},
+		opts = function()
+			local cmp = require("cmp")
+
+			return {
+				completion = { completeopt = "menu,menuone,preview,noinsert" },
+
+				-- For an understanding of why these mappings were
+				-- chosen, you will need to read `:help ins-completion`
+				--
+				-- No, but seriously. Please read `:help ins-completion`, it is really good!
+				mapping = cmp.mapping.preset.insert({
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+
+					-- Scroll the documentation window [b]ack / [f]orward
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+					-- If you prefer more traditional completion keymaps,
+					-- you can uncomment the following lines
+					--["<CR>"] = cmp.mapping.confirm { select = true },
+					--["<Tab>"] = cmp.mapping.select_next_item(),
+					--["<S-Tab>"] = cmp.mapping.select_prev_item(),
+
+					-- Manually trigger a completion from nvim-cmp.
+					--  Generally you don't need this, because nvim-cmp will display
+					--  completions whenever it has completion options available.
+					["<C-Space>"] = cmp.mapping.complete(),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+				}, {
+						{ name = "buffer" },
+					}),
+			}
+		end,
+	},
+
+	-- snippets
+	{
+		"hrsh7th/nvim-cmp",
+		optional = true,
+		dependencies = {
+			{
+				"garymjr/nvim-snippets",
+				opts = {
+					friendly_snippets = true,
+				},
+				dependencies = { "rafamadriz/friendly-snippets" },
+				-- native snippets. only needed on < 0.11, as 0.11 creates these by default
+				keys = {
+					{
+						"<Tab>",
+						function()
+							if vim.snippet.active({ direction = 1 }) then
+								vim.schedule(function()
+									vim.snippet.jump(1)
+								end)
+								return
+							end
+							return "<Tab>"
+						end,
+						expr = true,
+						silent = true,
+						mode = "i",
+					},
+					{
+						"<Tab>",
+						function()
+							vim.schedule(function()
+								vim.snippet.jump(1)
+							end)
+						end,
+						expr = true,
+						silent = true,
+						mode = "s",
+					},
+					{
+						"<S-Tab>",
+						function()
+							if vim.snippet.active({ direction = -1 }) then
+								vim.schedule(function()
+									vim.snippet.jump(-1)
+								end)
+								return
+							end
+							return "<S-Tab>"
+						end,
+						expr = true,
+						silent = true,
+						mode = { "i", "s" },
+					},
 				},
 			},
 		},
-		"saadparwaiz1/cmp_luasnip",
-		"hrsh7th/cmp-nvim-lsp",
-		"hrsh7th/cmp-path",
-		"hrsh7th/cmp-buffer",
-	},
-	opts = function()
-		local cmp = require("cmp")
-		local luasnip = require("luasnip")
-		luasnip.config.setup({})
-
-		return {
-			snippet = {
+		opts = function(_, opts)
+			opts.snippet = {
 				expand = function(args)
-					luasnip.lsp_expand(args.body)
+					return vim.snippet.expand(args.body)
 				end,
-			},
-			completion = { completeopt = "menu,menuone,preview,noinsert" },
+			}
 
-			-- For an understanding of why these mappings were
-			-- chosen, you will need to read `:help ins-completion`
-			--
-			-- No, but seriously. Please read `:help ins-completion`, it is really good!
-			mapping = cmp.mapping.preset.insert({
-				["<C-n>"] = cmp.mapping.select_next_item(),
-				["<C-p>"] = cmp.mapping.select_prev_item(),
-
-				-- Scroll the documentation window [b]ack / [f]orward
-				["<C-b>"] = cmp.mapping.scroll_docs(-4),
-				["<C-f>"] = cmp.mapping.scroll_docs(4),
-				["<CR>"] = cmp.mapping.confirm({ select = true }),
-
-				-- If you prefer more traditional completion keymaps,
-				-- you can uncomment the following lines
-				--["<CR>"] = cmp.mapping.confirm { select = true },
-				--["<Tab>"] = cmp.mapping.select_next_item(),
-				--["<S-Tab>"] = cmp.mapping.select_prev_item(),
-
-				-- Manually trigger a completion from nvim-cmp.
-				--  Generally you don't need this, because nvim-cmp will display
-				--  completions whenever it has completion options available.
-				["<C-Space>"] = cmp.mapping.complete({}),
-
-				-- Think of <c-l> as moving to the right of your snippet expansion.
-				--  So if you have a snippet that's like:
-				--  function $name($args)
-				--    $body
-				--  end
-				--
-				-- <c-l> will move you to the right of each of the expansion locations.
-				-- <c-h> is similar, except moving you backwards.
-				["<C-l>"] = cmp.mapping(function()
-					if luasnip.expand_or_locally_jumpable() then
-						luasnip.expand_or_jump()
-					end
-				end, { "i", "s" }),
-				["<C-h>"] = cmp.mapping(function()
-					if luasnip.locally_jumpable(-1) then
-						luasnip.jump(-1)
-					end
-				end, { "i", "s" }),
-
-				-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-				--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-			}),
-			sources = cmp.config.sources({
-				{ name = "nvim_lsp" },
-				{ name = "path" },
-			}, {
-				{ name = "buffer" },
-			}, {
-				{ name = "luasnip" },
-			}),
-		}
-	end,
+			table.insert(opts.sources, { name = "snippets" })
+		end,
+	},
 }
