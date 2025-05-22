7df41ef8574e3e5a90ed0ad81884d16be989f3df
Resi Dwi <37761892+residwi@users.noreply.github.com>
2025-05-22 13:43:32 +0700

    Remap class textobject keybinding to avoid conflicts with gitsigns.nav_hunk

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -78,18 +78,22 @@ return {
 					enable = true,
 					goto_next_start = {
 						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
+						["]k"] = "@class.outer",
 						["]a"] = "@parameter.inner",
 					},
-					goto_next_end = { ["]F"] = "@function.outer", ["]C"] = "@class.outer", ["]A"] = "@parameter.inner" },
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]K"] = "@class.outer",
+						["]A"] = "@parameter.inner",
+					},
 					goto_previous_start = {
 						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
+						["[k"] = "@class.outer",
 						["[a"] = "@parameter.inner",
 					},
 					goto_previous_end = {
 						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
+						["[K"] = "@class.outer",
 						["[A"] = "@parameter.inner",
 					},
 				},
