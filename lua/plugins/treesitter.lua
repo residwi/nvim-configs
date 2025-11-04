0c22ba3865d136fa624d39e2b5dd093ad2300f64
Resi Dwi <37761892+residwi@users.noreply.github.com>
2025-11-05 02:51:10 +0700

    [treesitter] start parser for event buffer
    Pass the event buffer to vim.treesitter.start so Treesitter is started for
    the correct buffer when handling buffer events

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -133,7 +133,7 @@ return {
 
           -- highlighting
           if enabled("highlight", "highlights") then
-            pcall(vim.treesitter.start)
+            pcall(vim.treesitter.start, ev.buf)
           end
 
           -- indents
