ec977916054cb78c6dfb285286387ebfb4d5d4b5
Resi Dwi <37761892+residwi@users.noreply.github.com>
2026-03-19 00:15:24 +0700

    fix(lsp): improve conform opts and execute command handling

--- lua/plugins/lsp/util.lua
+++ lua/plugins/lsp/util.lua
@@ -1,6 +1,6 @@
 ---@class lsp.util
 local M = {}
---
+
 M.kind_filter = {
   default = {
     "Class",
@@ -89,18 +89,14 @@ end
 
 ---@param opts? lsp.Client.format
 function M.format(opts)
-  opts = vim.tbl_deep_extend(
-    "force",
-    {},
-    opts or {},
-    Util.opts("nvim-lspconfig").format or {},
-    Util.opts("conform.nvim").format or {}
-  )
+  opts = vim.tbl_deep_extend("force", {}, opts or {}, Util.opts("nvim-lspconfig").format or {})
   local ok, conform = pcall(require, "conform")
   -- use conform for formatting with LSP when available,
   -- since it has better format diffing
   if ok then
-    opts.formatters = {}
+    -- It should be `nil`, otherwise it doesn't fetch options from `formatters_by_ft`,
+    -- see https://github.com/stevearc/conform.nvim/blob/5420c4b5ea0aeb99c09cfbd4fd0b70d257b44f25/lua/conform/init.lua#L417-L418
+    opts.formatters = nil
     conform.format(opts)
   else
     vim.lsp.buf.format(opts)
@@ -124,9 +120,18 @@ M.action = setmetatable({}, {
 ---@class LspCommand: lsp.ExecuteCommandParams
 ---@field open? boolean
 ---@field handler? lsp.Handler
+---@field filter? string|vim.lsp.get_clients.Filter
+---@field title? string
 
 ---@param opts LspCommand
 function M.execute(opts)
+  local filter = opts.filter or {}
+  filter = type(filter) == "string" and { name = filter } or filter
+  local buf = vim.api.nvim_get_current_buf()
+
+  ---@cast filter vim.lsp.get_clients.Filter
+  local client = vim.lsp.get_clients(Util.merge({}, filter, { bufnr = buf }))[1]
+
   local params = {
     command = opts.command,
     arguments = opts.arguments,
@@ -137,7 +142,8 @@ function M.execute(opts)
       params = params,
     })
   else
-    return vim.lsp.buf_request(0, "workspace/executeCommand", params, opts.handler)
+    vim.list_extend(params, { title = opts.title })
+    return client:exec_cmd(params, { bufnr = buf }, opts.handler)
   end
 end
 
